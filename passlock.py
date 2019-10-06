a644785555f9ac77223e11f0d498f3f1c4c7c8ff
root <root@localhost.localdomain>
2019-10-05 22:19:31 -0400

    initial commit

---
+++ passlock.py
@@ -0,0 +1,17 @@
+# class User:
+#     """
+#     create a class that generetes an instance of a user
+#     """
+#     user_list = []
+    
+    
+#     def __init__(self,username,password):
+#         """
+#         a method that defines the properties on the class object 
+#         """
+        
+#         self.username = username
+#         self.password = password
+        
+    
+        
