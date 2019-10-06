93b3529c91afd5ac1b0fad948d0fe10374fd5571
root <root@localhost.localdomain>
2019-10-05 22:19:48 -0400

    initial commit

---
+++ passlock_test.py
@@ -0,0 +1,12 @@
+# import unittest
+# from passlock import User
+
+# class UserTest(unittest.TestCase):
+#     def setUp(self):
+#         """
+#         a method that runs before the test
+#         """
+#         self.new_user = User("AlbertByrone","qwertykey")
+        
+# def test_init(self):
+    
