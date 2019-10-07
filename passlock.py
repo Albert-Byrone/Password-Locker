72cd8e15ec9282f1f87d525bc8a290b15d111f60
root <root@localhost.localdomain>
2019-10-07 08:09:37 -0400

    create a class object

--- passlock.py
+++ passlock.py
@@ -1,6 +1,14 @@
 import pyperclip
 import random
 import string
+
+class User:
+    """
+    create a class that generetes an instance of a user
+    """
+    user_list = []
+    
+    
     def __init__(self,username,password):
         """
         a method that defines the properties on the class object 
