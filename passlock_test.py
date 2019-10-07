4eca4efc2fde6a131002f0c70de4b1a1c4d8d78c
root <root@localhost.localdomain>
2019-10-07 08:21:14 -0400

    test case to check if the new instance of the user object has neen created

--- passlock_test.py
+++ passlock_test.py
@@ -15,6 +15,14 @@ class TestClass(unittest.TestCase):
         self.assertEqual(self.new_user.username,'AlbertByrone')
         self.assertEqual(self.new_user.password,'qwertykey')
         
+    def test_save_user(self):
+        '''
+        test case to check if the new instance of the user object has neen created
+        '''
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+        
 
 # class UserTest(unittest.TestCase):
 #     def setUp(self):
