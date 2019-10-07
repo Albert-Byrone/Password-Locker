f4cb04b4abb470f93abce268d716aabfe3f44f52
root <root@localhost.localdomain>
2019-10-07 08:08:17 -0400

    create a function that initializes the user object

--- passlock.py
+++ passlock.py
@@ -1,8 +1,11 @@
-# class User:
-#     """
-#     create a class that generetes an instance of a user
-#     """
-#     user_list = []
+    def __init__(self,username,password):
+        """
+        a method that defines the properties on the class object 
+        """
+        
+        self.username = username
+        self.password = password
+        
     
     
 #     def __init__(self,username,password):
